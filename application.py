f952b1965a9458d5570778f83aa4448a8647ffe2
Gabriel Charles <gabriel.charles@gmail.com>
2021-06-25 13:29:04 -0500

    HTML Page

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
+def index():
+    return render_template('index.html')
+
+@application.route('/hello')
 def hello_world():
     return 'Hola mundo, ahora notificaciones a Teams'
